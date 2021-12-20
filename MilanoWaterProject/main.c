94c006bc7373b9c8c78a76bd36f862fb0f495e41
Jay White <jay.v.white@att.net>
2021-12-20 14:36:23 -0600

    Initial Commit

---
+++ MilanoWaterProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MilanoWaterProject
+//
+//  Created by Jay White on 12/20/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
