43a884d85e2daee4498ac8ef46f17e73379c885f
Jay White <jay.v.white@att.net>
2022-01-01 16:02:22 -0600

    cleaning up code

--- Blynk/main.cpp
+++ Blynk/main.cpp
@@ -10,9 +10,9 @@
 #define BLYNK_DEBUG
 //#define BLYNK_PRINT stdout
 #ifdef RASPBERRY
-  #include <BlynkApiWiringPi.h>
+#include <BlynkApiWiringPi.h>
 #else
-  #include <BlynkApiLinux.h>
+#include <BlynkApiLinux.h>
 #endif
 #include <BlynkSocket.h>
 #include <BlynkOptionsParser.h>
@@ -69,24 +69,24 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
     int i;
     float* payloadptr;
     
-
+    
     printf("Recv Data Arrived - Topic Name: %s Payload length: %d\n", topicName, message->payloadlen);
     
     if (message->payloadlen != 0) {
-       
-          //printf("Subscriber Data Payload: ");
-          payloadptr = (float *)message->payload;
-          for(i=0; i < (message->payloadlen/4); i++)
-          {
-             blynk_payload[i] = *payloadptr++ ;
-             printf("%f ", blynk_payload[i]);
-          }
-          printf("\n");
-          MQTTClient_freeMessage(&message);
-          MQTTClient_free(topicName);
+        
+        //printf("Subscriber Data Payload: ");
+        payloadptr = (float *)message->payload;
+        for(i=0; i < (message->payloadlen/4); i++)
+        {
+            blynk_payload[i] = *payloadptr++ ;
+            printf("%f ", blynk_payload[i]);
+        }
+        printf("\n");
+        MQTTClient_freeMessage(&message);
+        MQTTClient_free(topicName);
     }
-   
-   return 1;
+    
+    return 1;
 }
 MQTTClient client;
 
@@ -95,9 +95,9 @@ void setup_mqtt()
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
     int rc;
     int ch;
-
+    
     rc = MQTTClient_create(&client, ADDRESS, CLIENTID,
-        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+                           MQTTCLIENT_PERSISTENCE_NONE, NULL);
     if (rc != 0)    {
         printf("Failed to create client, return code %d\n", rc);
         exit(EXIT_FAILURE);
@@ -120,8 +120,8 @@ void setup_mqtt()
         printf("Failed to subscribe, return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
-
-
+    
+    
     //return rc;
 }
 BLYNK_WRITE(V1)
@@ -131,69 +131,69 @@ BLYNK_WRITE(V1)
 BLYNK_WRITE(V16) {
     switch (param.asInt())
     {
-    case 1: // Item 1
-      controlWord1 = 0b00 ;
-      //printf("Pump 1 OFF selected\n");
-      break;
-    case 2: // Item 2
-      controlWord1 = 0b01 ;
-      //printf("Pump 1 ON selected\n");
-      break;
-    case 3: // Item 3
-      controlWord1 = 0b10 ;
-      //printf("Pump 1 AUTO selected\n");
-      break;
-    default:
-      printf("Unknown item selected\n");
+        case 1: // Item 1
+            controlWord1 = 0b00 ;
+            //printf("Pump 1 OFF selected\n");
+            break;
+        case 2: // Item 2
+            controlWord1 = 0b01 ;
+            //printf("Pump 1 ON selected\n");
+            break;
+        case 3: // Item 3
+            controlWord1 = 0b10 ;
+            //printf("Pump 1 AUTO selected\n");
+            break;
+        default:
+            printf("Unknown item selected\n");
     }
 }
 BLYNK_WRITE(V17) {
     switch (param.asInt())
     {
-    case 1: // Item 1
-      controlWord2 = 0b00 ;
-      //printf("Pump 2 OFF selected\n");
-      break;
-    case 2: // Item 2
-      controlWord2 = 0b01 ;
-      //printf("Pump 2 ON selected\n");
-      break;
-    case 3: // Item 3
-      controlWord2 = 0b10 ;
-      //printf("Pump 2 Auto selected\n");
-      break;
-    default:
-      printf("Unknown item selected\n");
+        case 1: // Item 1
+            controlWord2 = 0b00 ;
+            //printf("Pump 2 OFF selected\n");
+            break;
+        case 2: // Item 2
+            controlWord2 = 0b01 ;
+            //printf("Pump 2 ON selected\n");
+            break;
+        case 3: // Item 3
+            controlWord2 = 0b10 ;
+            //printf("Pump 2 Auto selected\n");
+            break;
+        default:
+            printf("Unknown item selected\n");
     }
 }
 BLYNK_WRITE(V18) {
     switch (param.asInt())
     {
-    case 1: // Item 1
-      controlWord3 = 0b00 ;
-      //printf("Pump 3 OFF selected\n");
-      break;
-    case 2: // Item 2
-      controlWord3 = 0b01 ;
-      //printf("Pump 3 ON selected\n");
-      break;
-    case 3: // Item 3
-      controlWord3 = 0b10 ;
-      //printf("Pump 3 AUTO selected\n");
-      break;
-    default:
-      printf("Unknown item selected\n");
+        case 1: // Item 1
+            controlWord3 = 0b00 ;
+            //printf("Pump 3 OFF selected\n");
+            break;
+        case 2: // Item 2
+            controlWord3 = 0b01 ;
+            //printf("Pump 3 ON selected\n");
+            break;
+        case 3: // Item 3
+            controlWord3 = 0b10 ;
+            //printf("Pump 3 AUTO selected\n");
+            break;
+        default:
+            printf("Unknown item selected\n");
     }
 }
 void setup()
 {
     printf("Connecting to Blynk: %s, %s, %d\n", serv, auth, port);
     Blynk.begin(auth, serv, port);
-   /* 
-    tmr.setInterval(1000, [](){
-      Blynk.virtualWrite(V0, BlynkMillis()/1000);
-    });
-    */
+    /*
+     tmr.setInterval(1000, [](){
+     Blynk.virtualWrite(V0, BlynkMillis()/1000);
+     });
+     */
 }
 
 
@@ -203,18 +203,18 @@ void loop()
     char *ledcolor[] = { "Green", "Blue", "Orange", "Red" } ; // to help with debug print
     
     char *ledcolorPalette[] = { "0x00ff00",   //green
-                                "0x0000FF",   //blue
-                                "0xffff00",   //orange
-                                "0xff0000" }; //red
-    char *pumpMenu[] = { "OFF",   
-                         "ON",   
-                         "AUTO",   
-                         "ERROR" };                                
+        "0x0000FF",   //blue
+        "0xffff00",   //orange
+        "0xff0000" }; //red
+    char *pumpMenu[] = { "OFF",
+        "ON",
+        "AUTO",
+        "ERROR" };
     /*
      * Jazz up the basic collors
      */
-    //Blynk.setProperty(3, "color", "0x01ffB0"); 
-                                      
+    //Blynk.setProperty(3, "color", "0x01ffB0");
+    
     Blynk.setProperty(5, "color", ledcolorPalette[(int)blynk_payload[16]]);  // Set LED Label to HEX colour
     Blynk.setProperty(6, "color", ledcolorPalette[(int)blynk_payload[17]]);  // Set LED Label to HEX colour
     Blynk.setProperty(7, "color", ledcolorPalette[(int)blynk_payload[18]]);  // Set LED Label to HEX colour
@@ -231,23 +231,23 @@ void loop()
     Blynk.setProperty(16, "labels", "OFF", "ON", "AUTO");
     Blynk.setProperty(17, "labels", "Off", "On", "Auto");
     Blynk.setProperty(18, "labels", "Off", "On", "Auto");
-   /* 
-    printf("LED 1 %s ",  ledcolor[(int)blynk_payload[16]]);  // Set LED Label to HEX colour
-    printf("LED 2 %s ",  ledcolor[(int)blynk_payload[17]]);  // Set LED Label to HEX colour
-    printf("LED 3 %s\n", ledcolor[(int)blynk_payload[18]]);
-    printf("LED 1 %s ",  ledcolor[(int)blynk_payload[19]]); 
-    printf("LED 2 %s",   ledcolor[(int)blynk_payload[20]]);
-    printf("LED 3 %s",   ledcolor[(int)blynk_payload[21]]);
-    printf("LED 4 %s\n", ledcolor[(int)blynk_payload[22]]); 
-    */
-     
-      
-/***  SEND INFO TO BLYNK     ***/              
+    /*
+     printf("LED 1 %s ",  ledcolor[(int)blynk_payload[16]]);  // Set LED Label to HEX colour
+     printf("LED 2 %s ",  ledcolor[(int)blynk_payload[17]]);  // Set LED Label to HEX colour
+     printf("LED 3 %s\n", ledcolor[(int)blynk_payload[18]]);
+     printf("LED 1 %s ",  ledcolor[(int)blynk_payload[19]]);
+     printf("LED 2 %s",   ledcolor[(int)blynk_payload[20]]);
+     printf("LED 3 %s",   ledcolor[(int)blynk_payload[21]]);
+     printf("LED 4 %s\n", ledcolor[(int)blynk_payload[22]]);
+     */
+    
+    
+    /***  SEND INFO TO BLYNK     ***/
     Blynk.virtualWrite(V0, blynk_payload[0]);  //Press Sensor Val
-    Blynk.virtualWrite(V1, blynk_payload[1]);  //Level ft                            
-    Blynk.virtualWrite(V2, blynk_payload[2]);  //Gallons                        
+    Blynk.virtualWrite(V1, blynk_payload[1]);  //Level ft
+    Blynk.virtualWrite(V2, blynk_payload[2]);  //Gallons
     Blynk.virtualWrite(V3, blynk_payload[3]);  //Level %
-    Blynk.virtualWrite(V4, blynk_payload[4]);  //Total GPM                 
+    Blynk.virtualWrite(V4, blynk_payload[4]);  //Total GPM
     Blynk.virtualWrite(V5, blynk_payload[5]); //LED 1 Well 1
     Blynk.virtualWrite(V6, blynk_payload[6]); //LED 2 Well 2
     Blynk.virtualWrite(V7, blynk_payload[7]); //LED 3 Well 3
@@ -291,56 +291,56 @@ int main(int argc, char* argv[])
     setup_mqtt();
     
     while(true) {
-       /*
-       printf("calling recv\n");
-       rc = MQTTClient_receive(client, &topicNamePtr, topicLenPtr, &messagePtr, 40000);
-       printf("Back from recv - Topic Name: %s  topic leng: %d\n", topicNamePtr, topicLen);
-       printf("Back from recv - status: %d message length: %d\n", rc, messagePtr->payloadlen);
-      */
-         
-       //if (rc == MQTTCLIENT_SUCCESS && messagePtr->payloadlen != 0) {
-          //printf("Message arrived\n");
-          //printf("     topic: %s\n", topicNamePtr);
-         // printf("   message: ");
-         // payloadptr = (float *)messagePtr->payload;
-         // for(i=0; i<messagePtr->payloadlen; i++)
-         // {
-         //    putchar(*payloadptr++);
-         // }
-         // putchar('\n');
-         //for(i=0; i <= (messagePtr->payloadlen/4); i++)
-         //{
-         //    blynk_payload[i] = *payloadptr++ ;
-             //printf("%0f ", blynk_payload[i]);
-         //}
-         //printf("\n");
+        /*
+         printf("calling recv\n");
+         rc = MQTTClient_receive(client, &topicNamePtr, topicLenPtr, &messagePtr, 40000);
+         printf("Back from recv - Topic Name: %s  topic leng: %d\n", topicNamePtr, topicLen);
+         printf("Back from recv - status: %d message length: %d\n", rc, messagePtr->payloadlen);
+         */
         
-         //MQTTClient_freeMessage(&messagePtr);
-         //MQTTClient_free(topicName);
-       //}
-       
-       loop();
-       /*
-        * Send APP Recv'd Data back to Subsriber
-        */
+        //if (rc == MQTTCLIENT_SUCCESS && messagePtr->payloadlen != 0) {
+        //printf("Message arrived\n");
+        //printf("     topic: %s\n", topicNamePtr);
+        // printf("   message: ");
+        // payloadptr = (float *)messagePtr->payload;
+        // for(i=0; i<messagePtr->payloadlen; i++)
+        // {
+        //    putchar(*payloadptr++);
+        // }
+        // putchar('\n');
+        //for(i=0; i <= (messagePtr->payloadlen/4); i++)
+        //{
+        //    blynk_payload[i] = *payloadptr++ ;
+        //printf("%0f ", blynk_payload[i]);
+        //}
+        //printf("\n");
+        
+        //MQTTClient_freeMessage(&messagePtr);
+        //MQTTClient_free(topicName);
+        //}
+        
+        loop();
+        /*
+         * Send APP Recv'd Data back to Subsriber
+         */
         controlWord = controlWord1 | (controlWord2 << 2) | (controlWord3 << 4);
         //printf("controlWord: %0x\n", controlWord);
         if (controlWord != controlWordLast) {
-           MQTTClient_message pubmsg = MQTTClient_message_initializer;
-           MQTTClient_deliveryToken token;
-   
-           pubmsg.payload = &controlWord;
-           pubmsg.payloadlen = 2;
-           pubmsg.qos = 0;
-           pubmsg.retained = 1;
-           printf("sending app data to subscriber\n");
-           MQTTClient_publishMessage(client, "Tank ESP", &pubmsg, &token);
-           controlWordLast = controlWord ;
-       }
+            MQTTClient_message pubmsg = MQTTClient_message_initializer;
+            MQTTClient_deliveryToken token;
+            
+            pubmsg.payload = &controlWord;
+            pubmsg.payloadlen = 2;
+            pubmsg.qos = 0;
+            pubmsg.retained = 1;
+            printf("sending app data to subscriber\n");
+            MQTTClient_publishMessage(client, "Tank ESP", &pubmsg, &token);
+            controlWordLast = controlWord ;
+        }
     }
     MQTTClient_unsubscribe(client, TOPIC);
     MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);  
+    MQTTClient_destroy(&client);
     return 0;
 }
 
