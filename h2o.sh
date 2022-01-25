3483bc360ec603ca382fea23c2f063f1ca78d9bc
Jay White <jay.v.white@att.net>
2022-01-25 08:09:51 -0600

    cron script

---
+++ h2o.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/usr/local/lib:/home/pi/lib:/home/pi/CodeDev/paho.mqtt.c-master/build/output:/home/pi/json-c/json-c-build
+echo "### Start the Processes  ###"
+echo "Starting Subscriber App"
+sleep 20
+nohup /home/pi/CodeDev/MilanoWaterProject/bin/subscriber > /dev/null 2>&1 &
+echo "Done"
+echo "Starting Monitor App"
+sleep 20
+nohum /home/pi/CodeDev/MilanoWaterProject/bin/monitor > /dev/null 2>&1 &
+sleep 20
+echo "Starting Blynk Interface"
+nohup /home/pi/CodeDev/MilanoWaterProject/bin/blynk > /dev/null 2>&1 &
+echo "Done"
+sleep 30m
