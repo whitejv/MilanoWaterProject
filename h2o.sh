24b8a321e56bb5f56bee7b7a0891f21cff138a84
Jay White <jay.v.white@att.net>
2022-01-28 10:51:09 -0600

    Update h2o.sh

--- h2o.sh
+++ h2o.sh
@@ -3,13 +3,13 @@ export LD_LIBRARY_PATH=/usr/local/lib:/home/pi/lib:/home/pi/CodeDev/paho.mqtt.c-
 echo "### Start the Processes  ###"
 echo "Starting Subscriber App"
 sleep 20
-nohup /home/pi/CodeDev/MilanoWaterProject/bin/subscriber > /dev/null 2>&1 &
+nohup /home/pi/MilanoWaterProject/bin/subscriber > /dev/null 2>&1 &
 echo "Done"
 echo "Starting Monitor App"
 sleep 20
-nohum /home/pi/CodeDev/MilanoWaterProject/bin/monitor > /dev/null 2>&1 &
+nohup /home/pi/MilanoWaterProject/bin/monitor > /dev/null 2>&1 &
 sleep 20
 echo "Starting Blynk Interface"
-nohup /home/pi/CodeDev/MilanoWaterProject/bin/blynk > /dev/null 2>&1 &
+nohup /home/pi/MilanoWaterProject/bin/blynk > /dev/null 2>&1 &
 echo "Done"
 sleep 30m
