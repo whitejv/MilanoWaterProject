83cd70a481a2b4087524cd5f93b4c9bb89b9496e
Jay White <jay.v.white@att.net>
2022-12-21 12:54:42 -0600

    Refactor of msgarrvd code

--- mylib/msgarrvd.c
+++ mylib/msgarrvd.c
@@ -7,50 +7,46 @@
 #include "MQTTClient.h"
 #include "../include/water.h"
 
+/*
+ * Cut and Paste Begins Here
+ */
 
- 
 int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
 {
-    time_t t;
-    time(&t);
-    int i;
-    unsigned short int* payloadptr;
-    float* sensorpayloadptr;
-
+   
+   int i;
+   unsigned short int* payloadptr;
+   float* sensorpayloadptr;
+   
+   
+   printf("Message arrived:\n");
+   printf("          topic: %s  ", topicName);
+   printf("         length: %d  ", topicLen);
+   printf("     PayloadLen: %d\n", message->payloadlen);
+   printf("message: ");
+   
+   if ( strcmp(topicName, FLO_TOPIC) {
+      memcpy(flow_data_payload, message->payload, topicLen)
+      for(i=0; i < (message->payloadlen/2); i++)
+      {
+         printf("%0x ", flow_data_payload[i]);
+      }
+      printf("|\n");
       
-    //printf("Message arrived:\n");
-    //printf("          topic: %s  ", topicName);
-    //printf("         length: %d  ", topicLen);
-    //printf("     PayloadLen: %d\n", message->payloadlen);
-    //printf("message: ");
-   
-   
-    if (message->payloadlen != 0) {
-         if ( message->payloadlen == 42) {
-            payloadptr = (unsigned short int *)message->payload;
-            for(i=0; i < (message->payloadlen/2); i++)
-            {
-               flow_data_payload[i] = *payloadptr++ ;
-               //printf("%0x ", flow_data_payload[i]);
-             }
-             printf("|\n");
-         }
-         else if ( F_LEN*4 == message->payloadlen) {
-            sensorpayloadptr = (float *)message->payload;
-            for(i=0; i < (message->payloadlen/4); i++)
-            {
-               formatted_sensor_payload[i] = *sensorpayloadptr++ ;
-               //printf("%0f ", formatted_sensor_payload[i]);
-             }
-             printf("+\n");
-         }
-         //printf("%s", ctime(&t));
-         
-         
-        MQTTClient_freeMessage(&message);
-        MQTTClient_free(topicName);
-    }
-    return 1;
+   }
+   else if ( strcmp(topicName, F_TOPIC)) {
+      memcpy(formatted_sensor_payload, message->payload, topicLen)
+      for(i=0; i < (message->payloadlen/4); i++)
+      {
+         printf("%0f ", formatted_sensor_payload[i]);
+      }
+      printf("+\n");
+   }
+       
+       
+       MQTTClient_freeMessage(&message);
+       MQTTClient_free(topicName);
+       return 1;
 }
- 
-
+       
+       
