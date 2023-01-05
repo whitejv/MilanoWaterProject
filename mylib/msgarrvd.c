8785ae4131fb389eaad3fc06ba53a5df7e4b1042
Jay White <jay.v.white@att.net>
2023-01-05 15:57:10 -0600

    Quiet Mode - No Prints

--- mylib/msgarrvd.c
+++ mylib/msgarrvd.c
@@ -16,41 +16,41 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
 {
    
    int i;
-   
+   /*
    printf("Message arrived:\n");
    printf("          topic: %s  ", topicName);
    printf("         length: %d  ", topicLen);
    printf("     PayloadLen: %d\n", message->payloadlen);
    printf("message: ");
-   
+   */
    if ( strcmp(topicName, FLO_TOPIC) == 0) {
       memcpy(flow_data_payload, message->payload, message->payloadlen);
-      for(i=0; i < FLO_LEN; i++) {printf("%0x ", flow_data_payload[i]);}
+      //for(i=0; i < FLO_LEN; i++) {printf("%0x ", flow_data_payload[i]);}
       printf("|\n");
    }
    else if ( strcmp(topicName, F_TOPIC) == 0) {
       memcpy(formatted_sensor_payload, message->payload, message->payloadlen);
-      for(i=0; i < F_LEN; i++) { printf("%0f ", formatted_sensor_payload[i]);}
+      //for(i=0; i < F_LEN; i++) { printf("%0f ", formatted_sensor_payload[i]);}
       printf("+\n");
    }
    else if ( strcmp(topicName, M_TOPIC) == 0) {
       memcpy(monitor_sensor_payload, message->payload, message->payloadlen);
-      for(i=0; i < M_LEN; i++) { printf("%0x ", monitor_sensor_payload[i]);}
+      //for(i=0; i < M_LEN; i++) { printf("%0x ", monitor_sensor_payload[i]);}
       printf(".\n");
    }
    else if ( strcmp(topicName, A_TOPIC) == 0) {
       memcpy(alert_sensor_payload, message->payload, message->payloadlen);
-      for(i=0; i < A_LEN; i++) {printf("%0x ", alert_sensor_payload[i]);}
+      //for(i=0; i < A_LEN; i++) {printf("%0x ", alert_sensor_payload[i]);}
       printf("*\n");
    }
    else if ( strcmp(topicName, FL_TOPIC) == 0) {
       memcpy(flow_sensor_payload, message->payload, message->payloadlen);
-      for(i=0; i < FL_LEN; i++) {printf("%0f ", flow_sensor_payload[i]);}
+      //for(i=0; i < FL_LEN; i++) {printf("%0f ", flow_sensor_payload[i]);}
       printf("^\n");
    }
    else if ( strcmp(topicName, ESP_TOPIC) == 0) {
       memcpy(data_payload, message->payload, message->payloadlen);
-      for(i=0; i < ESP_LEN; i++) {printf("%0x ", data_payload[i]);}
+      //for(i=0; i < ESP_LEN; i++) {printf("%0x ", data_payload[i]);}
       printf("-\n");
    }
    
