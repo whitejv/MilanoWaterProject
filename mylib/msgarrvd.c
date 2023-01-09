85b0055672669112cca6e8dd865318d42de1d1e6
Jay White <jay.v.white@att.net>
2023-01-09 10:07:27 -0600

    Revert "Fixed code using chatGPT"
    This reverts commit ee9d39e3ed82ba6408aed4268b08a1e4604c9288.
    
    	modified:   mylib/msgarrvd.c

--- mylib/msgarrvd.c
+++ mylib/msgarrvd.c
@@ -1,44 +1,62 @@
-
 /*
- * This function determines what message has arrived and copies the payload to the appropriate global variable.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include "unistd.h"
+#include "MQTTClient.h"
+#include "../include/water.h"
+*/
+/*
+ * Cut and Paste Begins Here
  */
 
-struct Payload {
-  char *data;
-  int len;
-};
-
-struct Payload payloads[] = {
-  {(char *)flow_data_payload, FLO_LEN},
-  {(char *)formatted_sensor_payload, F_LEN},
-  {(char *)monitor_sensor_payload, M_LEN},
-  {(char *)alert_sensor_payload, A_LEN},
-  {(char *)flow_sensor_payload, FL_LEN},
-  {(char *)data_payload, ESP_LEN}
-};
-
-const char *topics[] = {FLO_TOPIC, F_TOPIC, M_TOPIC, A_TOPIC, FL_TOPIC, ESP_TOPIC};
-
-int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
-  /*
-  printf("Message arrived:\n");
-  printf("          topic: %s  ", topicName);
-  printf("         length: %d  ", topicLen);
-  printf("     PayloadLen: %d\n", message->payloadlen);
-  printf("message: ");
-  */
-
-  for (int i = 0; i < sizeof(topics) / sizeof(topics[0]); i++) {
-    if (strcmp(topicName, topics[i]) == 0) {
-      struct Payload p = payloads[i];
-      memcpy(p.data, message->payload, message->payloadlen);
-      //for (int j = 0; j < p.len; j++) {printf("%0x ", p.data[j]);}
-      //printf("\n");
-      break;
-    }
-  }
-
-  MQTTClient_freeMessage(&message);
-  MQTTClient_free(topicName);
-}     
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+   
+   int i;
+   /*
+   printf("Message arrived:\n");
+   printf("          topic: %s  ", topicName);
+   printf("         length: %d  ", topicLen);
+   printf("     PayloadLen: %d\n", message->payloadlen);
+   printf("message: ");
+   */
+   if ( strcmp(topicName, FLO_TOPIC) == 0) {
+      memcpy(flow_data_payload, message->payload, message->payloadlen);
+      //for(i=0; i < FLO_LEN; i++) {printf("%0x ", flow_data_payload[i]);}
+      printf("|\n");
+   }
+   else if ( strcmp(topicName, F_TOPIC) == 0) {
+      memcpy(formatted_sensor_payload, message->payload, message->payloadlen);
+      //for(i=0; i < F_LEN; i++) { printf("%0f ", formatted_sensor_payload[i]);}
+      printf("+\n");
+   }
+   else if ( strcmp(topicName, M_TOPIC) == 0) {
+      memcpy(monitor_sensor_payload, message->payload, message->payloadlen);
+      //for(i=0; i < M_LEN; i++) { printf("%0x ", monitor_sensor_payload[i]);}
+      printf(".\n");
+   }
+   else if ( strcmp(topicName, A_TOPIC) == 0) {
+      memcpy(alert_sensor_payload, message->payload, message->payloadlen);
+      //for(i=0; i < A_LEN; i++) {printf("%0x ", alert_sensor_payload[i]);}
+      printf("*\n");
+   }
+   else if ( strcmp(topicName, FL_TOPIC) == 0) {
+      memcpy(flow_sensor_payload, message->payload, message->payloadlen);
+      //for(i=0; i < FL_LEN; i++) {printf("%0f ", flow_sensor_payload[i]);}
+      printf("^\n");
+   }
+   else if ( strcmp(topicName, ESP_TOPIC) == 0) {
+      memcpy(data_payload, message->payload, message->payloadlen);
+      //for(i=0; i < ESP_LEN; i++) {printf("%0x ", data_payload[i]);}
+      printf("-\n");
+   }
+   
+   MQTTClient_freeMessage(&message);
+   MQTTClient_free(topicName);
+   return 1;
+}
+       
        
