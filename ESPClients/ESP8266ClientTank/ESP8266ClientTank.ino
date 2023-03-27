fdc7626790a592d143517c494ae7ddf0befa2a9e
Jay White <jay.v.white@att.net>
2023-03-27 07:44:22 -0500

    clean up the IOT clients from 16bit to 32bit

--- ESPClients/ESP8266ClientTank/ESP8266ClientTank.ino
+++ ESPClients/ESP8266ClientTank/ESP8266ClientTank.ino
@@ -4,100 +4,39 @@
 #include <IPAddress.h>
 #include <PubSubClient.h>
 
-/* NCD ESP8266 Board - Select GENERIC ESP8266 MODULE
- * in Arduino Board Manager 
- */
- #if defined (ARDUINO_ESP8266_GENERIC)
-   #include <ESP8266WiFi.h>
-   #include <OneWire.h>
-   #include <DallasTemperature.h>
-#endif
-/* WEMOS D1R1 ESP8266 Board - Amazon 3/10/2023
- * in Arduino Board Manager 
- */
-#if defined (ARDUINO_ESP8266_WEMOS_D1R1)
-   #include <ESP8266WiFi.h>
-   #include <OneWire.h>
-   #include <DallasTemperature.h>
-#endif
-/* NCD ESP32 Board - Select Adafruit ESP32 Feather
- * in Arduino Board Manager 
- */
-#if defined (ARDUINO_FEATHER_ESP32)
-   #include <WiFi.h>
-   #include <esp_task_wdt.h>
-#endif
-/* RPI PICO W - Select ARDUINO_RASPBERRY_PI_PICO_W
- * in Arduino Board Manager 
- */
-#if defined (ARDUINO_RASPBERRY_PI_PICO_W)
-   #include <WiFi.h>
-   #include "pico/stdlib.h"
-   #include "hardware/i2c.h"
-   #include "pico/binary_info.h"
+#if defined(ARDUINO_ESP8266_GENERIC) || defined(ARDUINO_ESP8266_WEMOS_D1MINI)
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#elif defined(ARDUINO_FEATHER_ESP32)
+#include <WiFi.h>
+#include <esp_task_wdt.h>
+#elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
+#include <WiFi.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "pico/binary_info.h"
 #endif
 
 #define WDT_TIMEOUT 10  //10 seconds Watch Dog Timer (WDT)
 
-char ssid[] = "ATT9LCV8fL_2.4";    //local wifi network SSID
-char password[] = "6jhz7ai7pqy5";  //local network password
+const char ssid[] = "ATT9LCV8fL_2.4";
+const char password[] = "6jhz7ai7pqy5";
 
 IPAddress MQTT_BrokerIP(192, 168, 1, 249);
 const char *mqttServer = "raspberrypi.local";
 const int mqttPort = 1883;
 
-//const char *mqttServer = "soldier.cloudmqtt.com";
-//const int mqttPort = 15599;
-//const char *mqttUser = "zerlcpdf";
-//const char *mqttPassword = "OyHBShF_g9ya";
-
 WiFiClient espFlowClient;
-
 PubSubClient client(MQTT_BrokerIP, mqttPort, espFlowClient);
 
 int WDT_Interval = 0;
 unsigned int masterCounter = 0;
 
-/*
- * Data Block Interface Control
- */
+int raw_tank_data[22] = { 0 };
 
-/*
-* payload 0	Pulses Counted in Time Window
-* payload 1	Number of milliseconds in Time Window
-* payload 2	Flag 1=new data 0=stale data
-* payload 3	Pressure Sensor Analog Value
-* payload 4	 unused
-* payload 5	 unused
-* payload 6	 unused
-* payload 7	 unused
-* payload 8	 unused
-* payload 9	 unused
-* payload 10	 unused
-* payload 11	 unused
-* payload 12	 Cycle Counter 16bit Int
-* payload 13	 unused
-* payload 14	 unused
-* payload 15	 unused
-* payload 16	 unused
-* payload 17	Temperature in F
-* payload 18	 unused
-* payload 19	 unused
-* payload 20	 FW Version 4 Hex 
-*/	
-/*	
-* payload 21	 Last payload is Control Word From User
-*/ 	
+#define FLOWSENSOR 13
 
-
-int raw_flow_data[22] =   { 0, 0, 0, 0, 0,
-                                           0, 0, 0, 0, 0,
-                                           0, 0, 0, 0, 0,
-                                           0, 0, 0, 0, 0, 0,
-                                           0 };
-
-#define FLOWSENSOR  13
- 
 long currentMillis = 0;
 long previousMillis = 0;
 long millisecond = 0;
@@ -105,26 +44,18 @@ int interval = 2000;
 volatile byte pulseCount;
 byte pulse1Sec = 0;
 
-void IRAM_ATTR pulseCounter()
-{
+void IRAM_ATTR pulseCounter() {
   pulseCount++;
 }
-// GPIO where the DS18B20 is connected to
-const int oneWireBus = 2;     
 
-// Setup a oneWire instance to communicate with any OneWire devices
+const int oneWireBus = 2;
 OneWire oneWire(oneWireBus);
-
-// Pass our oneWire reference to Dallas Temperature sensor 
 DallasTemperature sensors(&oneWire);
 
 void setup() {
-
   Serial.begin(115200);
   delay(10);
-
-  // We start by connecting to a WiFi network
-
+  
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
@@ -143,8 +74,7 @@ void setup() {
 
   while (!client.connected()) {
     Serial.printf("Connecting to MQTT.....");
-    //if (client.connect("ESP8266FlowClient", mqttUser, mqttPassword))
-    if (client.connect("ESP8266FlowClient")) {
+    if (client.connect("ESP8266TankClient")) {
       Serial.printf("connected\n");
     } else {
       Serial.printf("failed with ");
@@ -152,106 +82,97 @@ void setup() {
       delay(2000);
     }
   }
+
   client.subscribe("ESP Control");
 
-#if defined (ARDUINO_FEATHER_ESP32)
+#if defined(ARDUINO_FEATHER_ESP32)
   Serial.printf("Configuring WDT...");
-  esp_task_wdt_init(WDT_TIMEOUT, true);  //enable panic so ESP32 restarts
-  esp_task_wdt_add(NULL);                //add current thread to WDT watch
+  esp_task_wdt_init(WDT_TIMEOUT, true);
+  esp_task_wdt_add(NULL);
   Serial.printf("Complete\n");
   Wire.begin();
-#endif
-#if defined (ARDUINO_ESP8266_GENERIC)
-  Wire.begin(12,14);
-  pinMode(FLOWSENSOR, INPUT_PULLUP);  
-  sensors.begin(); // Start the DS18B20 sensor
+#elif defined(ARDUINO_ESP8266_GENERIC) || defined(ARDUINO_ESP8266_WEMOS_D1MINI)
+  Wire.begin(12, 14);
+  pinMode(FLOWSENSOR, INPUT_PULLUP);
+  sensors.begin();
   attachInterrupt(digitalPinToInterrupt(FLOWSENSOR), pulseCounter, FALLING);
-#endif
-#if defined (ARDUINO_RASPBERRY_PI_PICO_W)
+#elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
   i2c_init(i2c_default, 100 * 1000);
   gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
   gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-  //gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-  //gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
   Wire.begin();
 #endif
-pulseCount = 0;
-previousMillis = 0;
+
+  pulseCount = 0;
+  previousMillis = 0;
 }
 void loop() {
-  int i;
-  int decimal;
+  updateWatchdog();
+  updateMasterCounter();
+  updateFlowData();
+  updateTemperatureData();
+  readAnalogInput();
+  processMqttClient();
+  publishFlowData();
+  printFlowData();
+  delay(500);
+}
 
-
-/* If I2C Errors Exceed 100 in 10 seconds then force a reboot to hopefully clear up */
-/*
-  if (((I2CPanicCount - last_I2CPanicCount) > 25) && (WDT_Interval >= WDT_TIMEOUT)) {
-    Serial.printf("%d %d %d\n", I2CPanicCount, last_I2CPanicCount, WDT_Interval);
-    while (1) {}  //force a reboot
-  }
-*/
+void updateWatchdog() {
   if (WDT_Interval++ > WDT_TIMEOUT) { WDT_Interval = 0; }
-
   #if defined (ARDUINO_FEATHER_ESP32)
   esp_task_wdt_reset();
   #endif
+}
 
-  //I2CComm();
-
+void updateMasterCounter() {
   ++masterCounter;
-
   if (masterCounter > 28800) {  //Force a reboot every 8 hours
     while (1) {};
   }
+}
+
+void updateFlowData() {
   currentMillis = millis();
   if (((currentMillis - previousMillis) > interval) && pulseCount > 0 ) {
     pulse1Sec = pulseCount;
     millisecond = millis() - previousMillis ;
-    raw_flow_data[0] = pulse1Sec ;
-    raw_flow_data[1] = millisecond ;
-    raw_flow_data[2] = 1;
+    raw_tank_data[0] = pulse1Sec ;
+    raw_tank_data[1] = millisecond ;
+    raw_tank_data[2] = 1;
     previousMillis = millis();
   } else {
-    raw_flow_data[2] = 0 ;
+    raw_tank_data[2] = 0 ;
   }
-  pulseCount =0 ;
+  pulseCount = 0;
+}
 
-
+void updateTemperatureData() {
   sensors.requestTemperatures(); 
   float temperatureF = sensors.getTempFByIndex(0);
-  //printf("%f \n", temperatureF) ;
-  raw_flow_data[17] = *((int *)&temperatureF);
-// store first 2 bytes of float into i[0]
-// be careful here!!! endianness matters
-// for little endian, use
-// i[0] = *(((int16_t *)&f) + 1);
+  raw_tank_data[17] = *((int *)&temperatureF);
+}
 
-//raw_flow_data[18] = *(((int16_t *)&temperatureF) + 1);
-// store last 2 bytes of float into i[1]
-// be careful here!!! endianness matters
-// for little endian, use
-// i[1] = *(((int16_t *)&f) + 0);
+void readAnalogInput() {
+  raw_tank_data[3] = analogRead(A0);
+}
 
-  
-  raw_flow_data[12] = masterCounter;
- // raw_sensor_data[16] = I2CPanicCount;
-  raw_flow_data[3] = analogRead(A0);  // This reads the analog in value
-  
+void processMqttClient() {
   client.loop();
+}
 
-  client.publish("Flow ESP", (byte *)raw_flow_data, 42);
+void publishFlowData() {
+  client.publish("Flow ESP", (byte *)raw_tank_data, 42);
+}
 
+void printFlowData() {
   Serial.printf("FLow Data: ");
-  for (i = 0; i <= 16; ++i) {
-    Serial.printf("%x ", raw_flow_data[i]);
+  for (int i = 0; i <= 16; ++i) {
+    Serial.printf("%x ", raw_tank_data[i]);
   }
-  for (i = 17; i <= 17; ++i) {
-    Serial.printf("%f ", *((float *)&raw_flow_data[i]));
-  }
-  for (i = 19; i <= 20; ++i) {
-    Serial.printf("%x ", raw_flow_data[i]);
+  Serial.printf("%f ", *((float *)&raw_tank_data[17]));
+  for (int i = 19; i <= 20; ++i) {
+    Serial.printf("%x ", raw_tank_data[i]);
   }
   Serial.printf("\n");
-
-  delay(500);
 }
