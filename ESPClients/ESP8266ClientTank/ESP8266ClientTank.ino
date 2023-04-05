81ef1cec2eb3246d4325c90bfe989492287676f2
Jay White <jay.v.white@att.net>
2023-04-05 14:16:23 -0500

    Updated the tank code - first actual use
    Updated for discretes and analogs

--- ESPClients/ESP8266ClientTank/ESP8266ClientTank.ino
+++ ESPClients/ESP8266ClientTank/ESP8266ClientTank.ino
@@ -6,6 +6,9 @@
 
 #if defined(ARDUINO_ESP8266_GENERIC) || defined(ARDUINO_ESP8266_WEMOS_D1MINI)
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #elif defined(ARDUINO_FEATHER_ESP32)
@@ -36,6 +39,12 @@ unsigned int masterCounter = 0;
 int raw_tank_data[22] = { 0 };
 
 #define FLOWSENSOR 13
+#define float1 4
+#define float2 5
+#define float3 12
+#define float4 14
+
+unsigned long timerOTA ;
 
 long currentMillis = 0;
 long previousMillis = 0;
@@ -54,22 +63,73 @@ DallasTemperature sensors(&oneWire);
 
 void setup() {
   Serial.begin(115200);
-  delay(10);
-  
+
+  Serial.println("Booting");
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
   }
+
   Serial.println("");
   Serial.print("WiFi connected -- ");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  
+  /*
+   * Adding OTA Support
+   */
+  // Port defaults to 8266
+  ArduinoOTA.setPort(8266);
 
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  
   client.setServer(MQTT_BrokerIP, mqttPort);
 
   while (!client.connected()) {
@@ -92,9 +152,13 @@ void setup() {
   Serial.printf("Complete\n");
   Wire.begin();
 #elif defined(ARDUINO_ESP8266_GENERIC) || defined(ARDUINO_ESP8266_WEMOS_D1MINI)
-  Wire.begin(12, 14);
+  //Wire.begin(12, 14); //only if you are using I2C
   pinMode(FLOWSENSOR, INPUT_PULLUP);
-  sensors.begin();
+  pinMode( float1, INPUT_PULLUP);
+  pinMode( float2, INPUT_PULLUP);
+  pinMode( float3, INPUT_PULLUP);
+  pinMode( float4, INPUT_PULLUP);
+  sensors.begin();// Start the DS18B20 sensor
   attachInterrupt(digitalPinToInterrupt(FLOWSENSOR), pulseCounter, FALLING);
 #elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
   i2c_init(i2c_default, 100 * 1000);
@@ -105,17 +169,24 @@ void setup() {
 
   pulseCount = 0;
   previousMillis = 0;
+
+  timerOTA = millis();
 }
 void loop() {
-  updateWatchdog();
-  updateMasterCounter();
-  updateFlowData();
-  updateTemperatureData();
-  readAnalogInput();
-  processMqttClient();
-  publishFlowData();
-  printFlowData();
-  delay(500);
+  ArduinoOTA.handle() ;
+  if (millis() - timerOTA > 500) {
+     updateWatchdog();
+     updateMasterCounter();
+     updateFlowData();
+     updateTemperatureData();
+     readAnalogInput();
+     readDigitalInput();
+     processMqttClient();
+     publishFlowData();
+     printFlowData();
+     //delay(500);
+     timerOTA = millis() ;
+  }
 }
 
 void updateWatchdog() {
@@ -157,6 +228,19 @@ void readAnalogInput() {
   raw_tank_data[3] = analogRead(A0);
 }
 
+void readDigitalInput() {
+  int float100 = 0;
+  int float75 = 0;
+  int float50 = 0;
+  int float25 = 0 ;
+  
+  float100 = digitalRead(float1) ;
+  float75  = digitalRead(float2) ;
+  float50  = digitalRead(float3) ;
+  float25  = digitalRead(float4) ;
+  raw_tank_data[4] = (float100 << 3) | (float75 << 2) | (float50 << 1) | float25;
+}
+
 void processMqttClient() {
   client.loop();
 }
@@ -166,7 +250,7 @@ void publishFlowData() {
 }
 
 void printFlowData() {
-  Serial.printf("FLow Data: ");
+  Serial.printf("Well #3 Data: ");
   for (int i = 0; i <= 16; ++i) {
     Serial.printf("%x ", raw_tank_data[i]);
   }
