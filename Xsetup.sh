80db163309b05276f0b4f07ae0fc09c69eba4fe6
Jay White <jay.v.white@att.net>
2023-10-08 08:09:08 -0500

    improved setup install script

--- Xsetup.sh
+++ Xsetup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -x
 
+# Determine original invoking user's home directory
+ORIGINAL_USER=${SUDO_USER:-$(logname)}
+USER_HOME=$(getent passwd $ORIGINAL_USER | cut -d: -f6)
+
 # Install OpenSSL for C/C++ programs
 sudo apt-get update
 sudo apt-get install -y libssl-dev xutils-dev
 
 # Install FTP Daemon and configure it
-mkdir -p /home/$USER/FTP/files
-chmod a-w /home/$USER/FTP
+# Adjusted the path to the vsftpd.conf file
 sudo apt-get install -y vsftpd
+sudo cp $USER_HOME/MilanoWaterProject/misc/vsftpd.conf /etc/
 sudo service vsftpd restart
 
 # Install JSON Lib, CMake, Mosquitto MQTT Service
@@ -28,5 +32,12 @@ sudo make install
 cd /etc/mosquitto
 echo "listener 1883
 allow_anonymous true" | sudo tee -a mosquitto.conf
+
+# Install Python LIBs & Py Rainbird Project
+git clone https://github.com/allenporter/pyrainbird.git
+cd pyrainbird
+pip install -r requirements_dev.txt --ignore-requires-python
+pip install . --ignore-requires-python
+pip install paho-mqtt
 
 echo "Setup script has finished executing."
